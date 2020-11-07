7b9446483181d97527eb9e0c4a736b3acb32b3d5
99shailesh <99shaileshu@gmail.com>
2020-11-07 18:32:33 +0530

    text editor

---
+++ TextEd/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace TextEd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fILEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nEWToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oPENToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sAVEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rUNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eXCUTEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eXITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.sAVEToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fILEToolStripMenuItem,
+            this.rUNToolStripMenuItem,
+            this.eXITToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(4, -3);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(13, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(169, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fILEToolStripMenuItem
+            // 
+            this.fILEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nEWToolStripMenuItem,
+            this.oPENToolStripMenuItem,
+            this.sAVEToolStripMenuItem,
+            this.sAVEToolStripMenuItem1});
+            this.fILEToolStripMenuItem.Name = "fILEToolStripMenuItem";
+            this.fILEToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
+            this.fILEToolStripMenuItem.Text = "FILE";
+            this.fILEToolStripMenuItem.Click += new System.EventHandler(this.fILEToolStripMenuItem_Click);
+            // 
+            // nEWToolStripMenuItem
+            // 
+            this.nEWToolStripMenuItem.Name = "nEWToolStripMenuItem";
+            this.nEWToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.nEWToolStripMenuItem.Size = new System.Drawing.Size(183, 26);
+            this.nEWToolStripMenuItem.Text = "NEW";
+            this.nEWToolStripMenuItem.Click += new System.EventHandler(this.nEWToolStripMenuItem_Click);
+            // 
+            // oPENToolStripMenuItem
+            // 
+            this.oPENToolStripMenuItem.Name = "oPENToolStripMenuItem";
+            this.oPENToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.oPENToolStripMenuItem.Size = new System.Drawing.Size(183, 26);
+            this.oPENToolStripMenuItem.Text = "OPEN";
+            this.oPENToolStripMenuItem.Click += new System.EventHandler(this.oPENToolStripMenuItem_Click);
+            // 
+            // sAVEToolStripMenuItem
+            // 
+            this.sAVEToolStripMenuItem.Name = "sAVEToolStripMenuItem";
+            this.sAVEToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.sAVEToolStripMenuItem.Size = new System.Drawing.Size(183, 26);
+            this.sAVEToolStripMenuItem.Text = "SAVE";
+            this.sAVEToolStripMenuItem.Click += new System.EventHandler(this.sAVEToolStripMenuItem_Click);
+            // 
+            // rUNToolStripMenuItem
+            // 
+            this.rUNToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eXCUTEToolStripMenuItem});
+            this.rUNToolStripMenuItem.Name = "rUNToolStripMenuItem";
+            this.rUNToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.rUNToolStripMenuItem.Text = "RUN";
+            // 
+            // eXCUTEToolStripMenuItem
+            // 
+            this.eXCUTEToolStripMenuItem.Name = "eXCUTEToolStripMenuItem";
+            this.eXCUTEToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.eXCUTEToolStripMenuItem.Size = new System.Drawing.Size(194, 26);
+            this.eXCUTEToolStripMenuItem.Text = "EXCUTE";
+            this.eXCUTEToolStripMenuItem.Click += new System.EventHandler(this.eXCUTEToolStripMenuItem_Click);
+            // 
+            // eXITToolStripMenuItem
+            // 
+            this.eXITToolStripMenuItem.Name = "eXITToolStripMenuItem";
+            this.eXITToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.eXITToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+            this.eXITToolStripMenuItem.Text = "EXIT";
+            this.eXITToolStripMenuItem.Click += new System.EventHandler(this.eXITToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AcceptsTab = true;
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox1.Location = new System.Drawing.Point(61, 50);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(774, 399);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(622, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "untitled";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.richTextBox2.Location = new System.Drawing.Point(4, 50);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(51, 399);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // sAVEToolStripMenuItem1
+            // 
+            this.sAVEToolStripMenuItem1.Name = "sAVEToolStripMenuItem1";
+            this.sAVEToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.sAVEToolStripMenuItem1.Size = new System.Drawing.Size(248, 26);
+            this.sAVEToolStripMenuItem1.Text = "SAVE_FILE";
+            this.sAVEToolStripMenuItem1.Click += new System.EventHandler(this.sAVEToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 461);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "  TextEd_CPP";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fILEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nEWToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oPENToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sAVEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rUNToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eXCUTEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eXITToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ToolStripMenuItem sAVEToolStripMenuItem1;
+    }
+}
+
---
+++ TextEd/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Data.SqlTypes;
+
+namespace TextEd
+{
+   
+    public partial class Form1 : Form
+    {
+        string fileName, path;
+        int count = 1,flag=0,space_count=0;
+        char last_char='!',second_char='!';
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void nEWToolStripMenuItem_Click(object sender, EventArgs e)//new button
+        {
+            Form1 f = new Form1();
+            f.Show();
+        }
+
+        private void eXITToolStripMenuItem_Click(object sender, EventArgs e)//exit button
+        {
+            this.Close();
+        }
+
+        private void sAVEToolStripMenuItem_Click(object sender, EventArgs e) // save file
+        {
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "cpp files (*.cpp)|*.cpp|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                path = saveFileDialog1.FileName;
+                byte[] plainDataArray = ASCIIEncoding.ASCII.GetBytes(richTextBox1.Text);
+                using (var fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    fileStream.Write(plainDataArray, 0, plainDataArray.GetLength(0));
+                    label1.Text = path;
+
+                }
+            }
+        }//END
+
+        private void sAVEToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string str = richTextBox1.Text;
+            System.IO.File.WriteAllText(label1.Text, str);
+        }
+        private void oPENToolStripMenuItem_Click(object sender, EventArgs e)// HOW TO OPEN FILE
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                //string fileName;
+
+                fileName = dlg.FileName;
+                if (File.Exists(fileName) == true)
+                {
+                    StreamReader objreader = new StreamReader(fileName);
+                    richTextBox1.Text = objreader.ReadToEnd();
+                    label1.Text = fileName;
+                    objreader.Close();
+
+                }
+            }
+
+        }//END
+
+        private void fILEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void eXCUTEToolStripMenuItem_Click(object sender, EventArgs e)//excute  code
+        {
+            string command ="";
+            string dir = label1.Text;
+
+            string[] folder = dir.Split('.');
+            if(folder[1]=="cpp")
+            {
+                command = command + "g++ " + dir + " -o " + folder[0] + "&";
+                command = command + folder[0] + ".exe";
+            }
+            else if(folder[1]=="py")
+            {
+                command = command + "python " + dir;
+            }
+            else 
+            {
+                command = command + "node " + dir;
+            }
+            System.Diagnostics.Process.Start("CMD.exe","/K"+command);
+        }
+
+        
+
+        // Auto completeation feature of brackets
+        
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {   /*
+            if(flag==0)
+            {
+                this.richTextBox2.Text += (count.ToString())+ Environment.NewLine;
+                count++;
+                flag = 1;
+            }
+            string currentText = this.richTextBox1.Text;
+            char ch='!';
+            second_char = ch;
+            if(currentText.Length>3)
+            {
+               ch = currentText[currentText.Length - 1];//get last char
+               second_char = currentText[currentText.Length - 3];
+               last_char = ch;
+            }
+            char nxtchar = nextchar(ch);
+            if (nxtchar!='!')
+            {
+                this.richTextBox1.Text += nxtchar;
+                richTextBox1.SelectionStart = richTextBox1.Text.Length-1;
+                richTextBox1.Focus();
+            }
+        */
+        }
+        
+        public char nextchar(char ch)
+        {
+            char chr='!';
+            Dictionary<char, char> brackets = new Dictionary<char, char>();
+            brackets.Add('(',')');
+            brackets.Add('[', ']');
+            brackets.Add('{', '}');
+            //brackets.Add('<', '>'); removed due to inconvience in cout<< 
+            brackets.Add('"', '"');
+            foreach (KeyValuePair<char, char> kvp in brackets)
+            {
+                if(kvp.Key==ch)
+                {
+                    chr = kvp.Value;
+                }
+            }
+            return chr; 
+        }
+        public void bracket(char ch)
+        {
+            char close = nextchar(ch);
+            int lenth_befor_brack= richTextBox1.SelectionStart;
+            if(close!='!')
+            {
+                richTextBox1.Text = this.richTextBox1.Text.Substring(0, lenth_befor_brack) + close + this.richTextBox1.Text.Substring(lenth_befor_brack);
+                richTextBox1.SelectionStart = lenth_befor_brack;
+                richTextBox1.Focus();
+            }
+
+        }
+        //line number display and auto closing of brakets
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar==(char)13)
+            {
+                this.richTextBox2.Text += (count.ToString()) + Environment.NewLine;
+                count++;
+                //space_manager();
+            }
+            bracket(e.KeyChar);
+            if (flag == 0)
+            {
+                this.richTextBox2.Text += (count.ToString()) + Environment.NewLine;
+                count++;
+                flag = 1;
+            }
+        }
+        //space management
+        public void space_manager()
+        {
+            int totalLines = this.richTextBox1.Lines.Length;
+            string lastLine = "",space_str="";
+
+            if (totalLines>0)
+            {
+                lastLine = this.richTextBox1.Lines[totalLines - 1];
+            }
+            for(int i=0;i<lastLine.Length;i++)
+            {
+                if(lastLine[i]==' ')
+                {
+                    break;
+                }
+                space_count++;
+            }
+            for(int i=0;i<space_count;i++)
+            {
+                space_str += " ";
+            }
+            if (second_char=='{')
+            {
+                space_str += "  ";
+                richTextBox1.SelectionStart = richTextBox1.Text.Length - 1;
+                richTextBox1.Focus();
+                //this.richTextBox1.Text += "\n" + space_str;
+                richTextBox1.Text=this.richTextBox1.Text.Substring(0,this.richTextBox1.Text.Length-1)+ space_str+"\n"+ this.richTextBox1.Text.Substring(this.richTextBox1.Text.Length - 1);
+                richTextBox1.SelectionStart = richTextBox1.Text.Length - 2;
+                richTextBox1.Focus();
+            }
+            else if(second_char==':')
+            {
+
+            }
+            else
+            {
+
+            }
+
+        }
+
+
+    }
+}
